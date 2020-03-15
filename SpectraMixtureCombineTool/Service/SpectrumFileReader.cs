1e2838752cd3aa7ea32b05ec2c5aa1d3efbfb42c
JackRead <jackaunir@gmail.com>
2020-03-15 23:04:24 +0000

    temp commit for upload

---
+++ SpectraMixtureCombineTool/Service/SpectrumFileReader.cs
@@ -0,0 +1,28 @@
+﻿using Aunir.SpectrumAnalysis2.Core.Connectors;
+using Aunir.SpectrumAnalysis2.Core.Default;
+using Aunir.SpectrumAnalysis2.FossConnector;
+using Aunir.SpectrumAnalysis2.Interfaces;
+using Aunir.SpectrumAnalysis2.Pretreatments;
+using Aunir.SpectrumAnalysis2.Pretreatments.PretreatmentGeneration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SpectraMixtureCombineTool.Service
+{
+    public class SpectrumFileReader
+    {
+        public void Read(Stream stream)
+        {
+            var x = new FossSpectraReader();
+            var spectra = x.ReadStream(stream);
+            foreach(var spectrum in spectra)
+            {
+                foreach(var dataPoint in spectrum.Data)
+                {
+                }
+            }
+        }        
+    }
+}
