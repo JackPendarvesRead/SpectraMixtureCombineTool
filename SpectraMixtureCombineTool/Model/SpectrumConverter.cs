357393c1f7a0dbd2f72d66f53c852b73ef229b43
JackRead <jackaunir@gmail.com>
2020-03-16 09:18:29 +0000

    Added implementation to read and convert spectra

---
+++ SpectraMixtureCombineTool/Model/SpectrumConverter.cs
@@ -0,0 +1,58 @@
+﻿using Aunir.SpectrumAnalysis2.FossConnector;
+using Aunir.SpectrumAnalysis2.Interfaces;
+using Aunir.SpectrumAnalysis2.JcampConnector;
+using SpectraMixtureCombineTool.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SpectraMixtureCombineTool.Model
+{
+    public class SpectrumConverter
+    {
+        public ISpectrumData GetWeightedSpectra(IEnumerable<SpectraFileViewModel> files)
+        {
+            var spectra = ReadFiles(files);
+            return AggregateSpectra(spectra);
+        }
+
+        private IEnumerable<ISpectrumData> ReadFiles(IEnumerable<SpectraFileViewModel> files)
+        {
+            var reader = new FossSpectraReader();
+            foreach (var file in files)
+            {
+                using (var stream = new FileStream(file.FilePath, FileMode.Open, FileAccess.Read, FileShare.Delete))
+                {
+                    var spectrum = reader.ReadStream(stream).First();
+                    for (var i = 0; i < spectrum.Data.Count; i++)
+                    {
+                        spectrum.Data[i] *= float.Parse(file.Coefficient);
+                    }
+                    spectrum.SpectrumInformation.Add(file.Name, file.Coefficient);
+                    yield return spectrum;
+                }
+            }
+        }
+
+        private ISpectrumData AggregateSpectra(IEnumerable<ISpectrumData> spectra)
+        {
+            var data = spectra.Select(x => x.Data)
+                .Aggregate((acc, next) => acc.Zip(next, (a, b) => a + b).ToList());
+
+            var wavelengths = spectra.Select(x => x.Wavelengths)
+                .First();
+
+            var dic = spectra.Select(x => x.SpectrumInformation)
+                .Merge();
+
+            return new SpectrumData
+            {
+                Data = data,
+                Wavelengths = wavelengths,
+                SpectrumInformation = dic
+            };
+        }
+    }
+}
---
+++ SpectraMixtureCombineTool/Model/SpectrumData.cs
@@ -0,0 +1,31 @@
+﻿using Aunir.SpectrumAnalysis2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpectraMixtureCombineTool.Model
+{
+    public class SpectrumData : ISpectrumData
+    {
+        public IList<float> Data { get; set; }
+        public IList<float> Wavelengths { get; set; }
+        public IDictionary<string, string> SpectrumInformation { get; set; }
+    }
+
+    public static class SpectrumDataExtension
+    {
+        public static IDictionary<string, string> Merge(this IEnumerable<IDictionary<string, string>> dics)
+        {
+            var map = new Dictionary<string, string>();
+            foreach(var dic in dics)
+            {
+                foreach(var pair in dic)
+                {
+                    map[pair.Key] = pair.Value;
+                }
+            }
+            return map;
+        }
+    }
+}
