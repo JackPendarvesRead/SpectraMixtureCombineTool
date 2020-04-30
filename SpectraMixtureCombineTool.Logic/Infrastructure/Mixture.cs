9c9e1751bff41a954b69939f366adcf9e8714c87
JackRead <jackaunir@gmail.com>
2020-04-30 11:13:11 +0100

    removed commented code

--- SpectraMixtureCombineTool.Logic/Infrastructure/Mixture.cs
+++ SpectraMixtureCombineTool.Logic/Infrastructure/Mixture.cs
@@ -15,13 +15,6 @@ namespace SpectraMixtureCombineTool.Logic.Infrastructure
 
         public List<SpectrumData> Spectra { get; }
 
-
-        //public IEnumerable<SpectrumData> IngredientData => Spectra.Where(x => x.FileType == SpectraFileType.Ingredient);
-        //public IEnumerable<SpectrumData> FillerData => Spectra.Where(x => x.FileType == SpectraFileType.Filler);
-        //public IEnumerable<SpectrumData> ConstantData => Spectra.Where(x => x.FileType == SpectraFileType.Constant);
-
-        //public float CoefficientSum => Spectra.Select(x => x.RatioValue).Sum();
-
         public int IngredientCount => Spectra.Where(x => x.FileType == SpectraFileType.Ingredient).Count();
         public int ConstantCount => Spectra.Where(x => x.FileType == SpectraFileType.Constant).Count();
         public int FillerCount => Spectra.Where(x => x.FileType == SpectraFileType.Filler).Count();
